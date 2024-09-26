ce5edc41b31baa95005e1ec9a00873f5f6fad94b
basha-github <sharifabee.alur@gmail.com>
2024-09-26 13:41:05 +0530

    v1

--- StaticDemo.java
+++ StaticDemo.java
@@ -1,9 +1,33 @@
-
+class Student{
+	int id;
+	String name;
+	static String collegeName= "Awanshi";
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+		
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", collegeName=" + collegeName + "]";
+	}
+}
 public class StaticDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Student s1 = new Student(123,"Ramu");
+		Student s2 = new Student(456,"Raju");
+		
+		
+		Student.collegeName = "Avanthi";
+		//s2.collegeName = "Avanthi";
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
 	}
 
 }
